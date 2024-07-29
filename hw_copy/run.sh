746ccbf1a1a9f431003953a37044f99bda6a628e
ONLOX <3316820553@qq.com>
2024-07-29 16:41:43 +0800

    build: initial commit

---
+++ hw_copy/run.sh
@@ -0,0 +1,1 @@
+# Add your commands here
---
+++ src/add.cpp
@@ -0,0 +1,17 @@
+// add.cpp
+#include <iostream>
+int main(int argc, char* argv[])
+{
+    if (argc < 3)
+    {
+        std::cout << "Usage: " << argv[0] << " <num1> <num2>" << std::endl;
+        return 1;
+    }
+    else
+    {
+        int num1 = atoi(argv[1]);
+        int num2 = atoi(argv[2]);
+        std::cout << num1 << " + " << num2 << " = " << num1 + num2 << std::endl;
+    }
+    return 0;
+}
