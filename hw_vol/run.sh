536933870aaa9ab0a4033548bda5c8128820fab2
JaderOx <377964735@qq.com>
2024-07-31 13:09:36 +0800

    docker_牛浩瑜_无36_1

--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,5 @@
 # Add your commands here
+docker build -t hwtest:2.0 -f ./hw_vol/Dockerfile ..
+docker run -it --mount type=bind,source=./src,target=/workspace hwtest:2.0  bash
+g++ ./workspace/mul.cpp -o mul && ./mul 3 4 && cp mul /workspace
+exit
