43c9142a616178a61618125cd99e724da6a33a0a
SummerSanches <103926258+SummerSanches@users.noreply.github.com>
2024-08-03 06:37:38 +0800

    Update run.sh

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,17 @@
-# Add your commands here
+# Use an official Ubuntu as the base image
+FROM ubuntu:20.04
+
+# Install required packages
+RUN apt-get update && apt-get install -y g++ make
+
+# Copy the source code into the container
+COPY src/ /usr/src/
+
+# Set the working directory
+WORKDIR /usr/src/
+
+# Compile the programs
+RUN g++ add.cpp -o add && g++ mul.cpp -o mul
+
+# Specify the command to run the "add" program by default
+CMD ["./add"]
