287ea61a745127283305d87e593ea7d5a010ea4a
konpoku <543329382@qq.com>
2024-08-07 23:44:57 +0800

    updated

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,4 @@
-# Add your commands here
+#!/bin/zsh
+docker build -t hw_copy:1.0 ..
+docker run --rm hw_copy:1.0 /the/workdir/path/add 3 4
+docker run --rm hw_copy:1.0 /the/workdir/path/mul 3 4
--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,3 @@
-# Add your commands here
+docker build -t hw_vol:1.0 .
+docker run --rm --mount type=bind,source="$(pwd)"/src,target=/the/workdir/path/src hw_vol:1.0 \
+     sh -c 'g++ -o /the/workdir/path/src/add /the/workdir/path/src/add.cpp && /the/workdir/path/src/add 3 4'
