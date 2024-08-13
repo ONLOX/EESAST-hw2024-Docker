af3c9bb206b959933c2baff471dbd5111247c37d
guix-thu <guixin123456@outlook.com>
2024-08-14 01:52:57 +0900

    hw.

--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,6 @@
-# Add your commands here
+#!/bin/bash
+docker build -t hw_vol . -f ./hw_vol/Dockerfile
+docker run --rm --mount type=bind,source="$(pwd)"/src,target=/usr/src hw_vol:1.0 \
+     sh -c 'g++ -o ./add ./add.cpp && ./add 1 2'
+docker run --rm --mount type=bind,source="$(pwd)"/src,target=/usr/src hw_vol:1.0 \
+     sh -c 'g++ -o ./mul ./mul.cpp && ./mul 3 4'
