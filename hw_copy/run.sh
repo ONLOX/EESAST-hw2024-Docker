ec322ca2da5510a2e529c6bc86e3782d48f2c7c1
EM_Cai23 <160403721+EM-Cai23@users.noreply.github.com>
2024-08-16 10:09:15 +0800

    Hw_docker

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,9 @@
-# Add your commands here
+#! /bin/sh
+
+if ["$1" = "add"]; then
+    ./add
+elif ["$1" = "mul"]; then
+    ./mul
+else
+    echo "Please specify 'add' or 'mul' to run program."
+fi
--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,10 @@
-# Add your commands here
+#! /bin/sh
+
+# Compile
+g++ -o program "$1"
+
+# Run the program with arguments
+./program "${@:2}"
+
+# Move the compiled file back to the mounted directory
+mv program /mnt/output/
