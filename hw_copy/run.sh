a4ae509ca1d02ae0bb74b68d7814f64d1fc47f44
Your Name <you@example.com>
2024-08-18 16:40:28 +0800

    finish hw2024

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,7 @@
-# Add your commands here
+#!/bin/bash
+if [ "$1" == "add" ]; then
+    ./add "${@:2}"
+else
+    echo "Usage: $0 add <num1> <num2>"
+    exit 1
+fi
--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,8 @@
-# Add your commands here
+#!/bin/bash
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <cpp_file> <output_file> [args...]"
+    exit 1
+fi
+g++ -o "$2" "$1"
+"./$2" "${@:3}"
+cp "$2" /usr/src/myapp/output/
