8dcf9a479bc56da336d88448593fd5f1cc718c27
Your Name <you@example.com>
2024-08-18 17:26:00 +0800

    final commit

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 docker build -t myapp_hw_copy .
-
-docker run --rm myapp_hw_copy ./add "$@"
+if ["$1"=="add"]; then
+    docker run --rm myapp_hw_copy ./add "$@"
+elif["$1"=="mul"]; then
+    docker run --rm myapp_hw_copy ./mul "$@"
+else
+    echo "Invalid argument"
+fi
--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker build -t myapp_hw_vol .
-
-docker run --rm -v ../src:/usr/src/myapp -v ../output:/usr/src/myapp/output myapp_hw_vol bash -c "g++ -o add add.cpp && ./add $@ && cp add /usr/src/myapp/output/"
+docker run --rm -v ../src:/usr/src/myapp -v ../output:/usr/src/myapp/output myapp_hw_vol bash -c 'g++ -o "$1" "$1.cpp" && ./"$1" "${@:2}" && cp "$1" /usr/src/myapp/output/'
