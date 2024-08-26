47b4c9a6657f7bbeea679422505dc7160e8116fc
shaoqy823 <shaoqy23@mails.tsinghua.edu.cn>
2024-08-26 12:09:29 +0800

    docker_邵卿宇_无35

--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,5 @@
-# Add your commands here
+docker build -t hw_vol_image -f hw_vol/Dockerfile .
+docker run -v $(pwd)/src:/usr/src/app hw_vol_image g++ /usr/src/app/add.cpp -o /usr/src/app/add
+docker run -v $(pwd)/src:/usr/src/app hw_vol_image g++ /usr/src/app/add 1 2
+docker run -v $(pwd)/src:/usr/src/app hw_vol_image g++ /usr/src/app/mul.cpp -o /usr/src/app/mul
+docker run -v $(pwd)/src:/usr/src/app hw_vol_image g++ /usr/src/app/mul 1 2
