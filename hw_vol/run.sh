de9a1d3c1c8f1b1022e6e2adb647a305ed9e59c6
wang-tz <wang-tz23@mails.tsinghua.edu.cn>
2024-08-28 00:10:21 +0800

    v4

--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,3 +1,5 @@
 # Add your commands here
 docker build -t hw_vol:1.0 -f ./hw_vol/Dockerfile .
 docker run -it -P --name vol --mount type=bind,source=/mnt/c/Users/天泽/Desktop/软件部暑期培训/EESAST-hw2024-Docker/src,target=/root/src hw_vol:1.0 bash
+g++ /root/src/add.cpp -o add.exe ; ./add.exe 1 2
+docker cp vol:./add.exe /mnt/c/Users/天泽/Desktop/软件部暑期培训/EESAST-hw2024-Docker/src
